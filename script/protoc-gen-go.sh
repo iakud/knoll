0815c4b0ac80cb73b04fe15888f10e3c4870a318
iakud <kaikai87111@gmail.com>
2025-03-06 12:13:34 +0800

    更新protoc

--- script/protoc-gen-go.sh
+++ script/protoc-gen-go.sh
@@ -32,28 +32,12 @@ function printUsage() {
     $colorful && tput setaf 7
 }
 
-
-export http_proxy=http://127.0.0.1:1087
-export https_proxy=http://127.0.0.1:1087
+#export http_proxy=http://127.0.0.1:1087
+#export https_proxy=http://127.0.0.1:1087
 
 INSTALL_DIR="../local"
-PROTOC_GEN_GO_VERSION=1.36.1
+PROTOC_GEN_GO_VERSION=1.36.5
+PROTOC_GEN_GO_GRPC_VERSION=1.5.1
 
-if [[ `$INSTALL_DIR/bin/protoc-gen-go --version 2>&1 | grep -e "protoc-gen-go v$PROTOC_GEN_GO_VERSION"` ]]; then
-	echo -e "[misc] \033[0;33mprotoc-gen-go v$PROTOC_GEN_GO_VERSION\033[0;37m is already installed."
-	exit 0
-fi
-
-rm -rf $INSTALL_DIR"/bin/protoc-gen-go"
-mkdir -p $INSTALL_DIR"/bin"
-
-echo -e "[misc] Start to install \033[0;33mprotoc-gen-go $PROTOC_VERSION\033[0;37m..."
-wget -c "https://github.com/protocolbuffers/protobuf-go/releases/download/v$PROTOC_GEN_GO_VERSION/protoc-gen-go.v$PROTOC_GEN_GO_VERSION.darwin.arm64.tar.gz"
-[[ $? -ne 0 ]] && exit 1
-
-tar -xvf "protoc-gen-go.v$PROTOC_GEN_GO_VERSION.darwin.arm64.tar.gz" -C $INSTALL_DIR"/bin"
-[[ $? -ne 0 ]] && exit 1
-
-rm "protoc-gen-go.v$PROTOC_GEN_GO_VERSION.darwin.arm64.tar.gz"
-
-go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+hash protoc-gen-go 2>/dev/null || go install google.golang.org/protobuf/cmd/protoc-gen-go@v$PROTOC_GEN_GO_VERSION
+hash protoc-gen-go-grpc 2>/dev/null || go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v$PROTOC_GEN_GO_GRPC_VERSION
--- script/protoc.sh
+++ script/protoc.sh
@@ -33,11 +33,11 @@ function printUsage() {
 }
 
 
-export http_proxy=http://127.0.0.1:1087
-export https_proxy=http://127.0.0.1:1087
+#export http_proxy=http://127.0.0.1:1087
+#export https_proxy=http://127.0.0.1:1087
 
 INSTALL_DIR="../local"
-PROTOC_VERSION=29.2
+PROTOC_VERSION=30.0
 
 if [[ `$INSTALL_DIR/protoc/bin/protoc --version 2>&1 | grep -e "libprotoc $PROTOC_VERSION"` ]]; then
 	echo -e "[misc] \033[0;33mprotoc $PROTOC_VERSION\033[0;37m is already installed."
