a973b65d4a63bc1e049e4c7ed25caa6304bb8819
iakud <kaikai87111@gmail.com>
2025-06-09 00:04:32 +0800

    修正java path

--- var.sh
+++ var.sh
@@ -1,9 +1,5 @@
 PWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# export PATH="`brew --prefix openjdk@11`/bin:$PATH"
-
-PWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 LOCAL_PATH=$PWD/local
 LOCAL_BIN=$LOCAL_PATH/bin
 LOCAL_INCLUDE=$LOCAL_PATH/include
@@ -13,3 +9,7 @@ PROTOC_BIN=$PROTOC_PATH/bin
 PROTOC_INCLUDE=$PROTOC_PATH/include
 
 PATH=$LOCAL_BIN:$PROTOC_BIN:$PATH
+
+if [[ `uname -m | grep -e "arm64"` ]]; then
+	PATH="`brew --prefix openjdk@11`/bin:$PATH"
+fi
