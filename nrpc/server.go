06a1eab411596efd750553feb913e3693dc232e3
iakud <kaikai87111@gmail.com>
2025-10-24 18:30:05 +0800

    nrpc server

--- nrpc/server.go
+++ nrpc/server.go
@@ -2,6 +2,7 @@ package nrpc
 
 import (
 	"context"
+	"fmt"
 	"reflect"
 	"strings"
 	"sync"
@@ -39,6 +40,7 @@ type Server struct {
 	mu   sync.Mutex // guards following
 	nc   *nats.Conn
 	subj string
+	sub  *nats.Subscription
 
 	services map[string]*serviceInfo
 }
@@ -56,8 +58,7 @@ func (s *Server) RegisterService(sd *ServiceDesc, ss any) {
 		ht := reflect.TypeOf(sd.HandlerType).Elem()
 		st := reflect.TypeOf(ss)
 		if !st.Implements(ht) {
-			return
-			// panic("nrpc: Server.RegisterService found the handler of type %v that does not satisfy %v", st, ht)
+			panic(fmt.Sprintf("nrpc: Server.RegisterService found the handler of type %v that does not satisfy %v", st, ht))
 		}
 	}
 	s.register(sd, ss)
@@ -66,15 +67,12 @@ func (s *Server) RegisterService(sd *ServiceDesc, ss any) {
 func (s *Server) register(sd *ServiceDesc, ss any) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
-	/*
-		s.printf("RegisterService(%q)", sd.ServiceName)
-		if s.serve {
-			panic("nrpc: Server.RegisterService after Server.Serve for %q", sd.ServiceName)
-		}
-	*/
+
+	if s.sub != nil {
+		panic(fmt.Sprintf("nrpc: Server.RegisterService after Server.Serve for %q", sd.ServiceName))
+	}
 	if _, ok := s.services[sd.ServiceName]; ok {
-		return
-		// fmt.fprintf("grpc: Server.RegisterService found duplicate service registration for %q", sd.ServiceName)
+		panic(fmt.Sprintf("grpc: Server.RegisterService found duplicate service registration for %q", sd.ServiceName))
 	}
 	info := &serviceInfo{
 		serviceImpl: ss,
@@ -96,6 +94,8 @@ func (s *Server) register(sd *ServiceDesc, ss any) {
 }
 
 func (s *Server) Start() error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
 	sub, err := s.nc.Subscribe(s.subj, func(msg *nats.Msg) {
 		ctx := context.TODO()
 		sm := msg.Header.Get("method")
@@ -112,29 +112,49 @@ func (s *Server) Start() error {
 		srv, knownService := s.services[service]
 		if knownService {
 			if md, ok := srv.methods[method]; ok {
-				df := func(v interface{}) error {
-					return proto.Unmarshal(msg.Data, v.(proto.Message))
-				}
-				reply, err := md.Handler(srv.serviceImpl, ctx, df)
-				if err != nil {
-					// FIXME: err
-					return
-				}
-				data, err := proto.Marshal(reply.(proto.Message))
-				if err != nil {
-					// FIXME: err
-					return
-				}
-				if err := msg.Respond(data); err != nil {
-					// FIXME: err
-				}
+				s.processMsg(ctx, msg, srv, md)
+				return
 			}
 		}
 	})
-	_ = sub
-	// sub.Unsubscribe()
+
 	if err != nil {
 		return err
 	}
+	s.sub = sub
 	return nil
+}
+
+func (s *Server) Close() error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	sub := s.sub
+	if sub == nil {
+		return nil
+	}
+	err := sub.Unsubscribe()
+	if err != nil {
+		return err
+	}
+	s.sub = nil
+	return nil
+}
+
+func (s *Server) processMsg(ctx context.Context, msg *nats.Msg, srv *serviceInfo, md *MethodDesc) {
+	df := func(v interface{}) error {
+		return proto.Unmarshal(msg.Data, v.(proto.Message))
+	}
+	reply, err := md.Handler(srv.serviceImpl, ctx, df)
+	if err != nil {
+		// FIXME: err
+		return
+	}
+	data, err := proto.Marshal(reply.(proto.Message))
+	if err != nil {
+		// FIXME: err
+		return
+	}
+	if err := msg.Respond(data); err != nil {
+		// FIXME: err
+	}
 }
