1b1fe8667f15c5e8ead316e6c400137f3b39e5f9
iakud <kaikai87111@gmail.com>
2025-11-19 14:25:31 +0800

    uuid node

--- uuid/uuid.go
+++ uuid/uuid.go
@@ -11,9 +11,15 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var (
+const (
 	NodeBits uint8 = 16
 	StepBits uint8 = 12
+
+	nodeMax       = -1 ^ (-1 << NodeBits)
+	nodeMask      = nodeMax << StepBits
+	stepMask      = -1 ^ (-1 << StepBits)
+	sequenceShift = NodeBits + StepBits
+	nodeShift     = StepBits
 )
 
 type Node struct {
@@ -23,30 +29,17 @@ type Node struct {
 	step     int64
 
 	coll *mongo.Collection
-	tag  string
-
-	nodeMax       int64
-	nodeMask      int64
-	stepMask      int64
-	sequenceShift uint8
-	nodeShift     uint8
+	kind string
 }
 
-type ID int64
-
-func NewNode(node int64, coll *mongo.Collection, tag string) (*Node, error) {
+func NewNode(node int64, coll *mongo.Collection, kind string) (*Node, error) {
 	n := Node{}
 	n.node = node
-	n.tag = tag
+	n.kind = kind
 	n.coll = coll
-	n.nodeMax = -1 ^ (-1 << NodeBits)
-	n.nodeMask = n.nodeMax << StepBits
-	n.stepMask = -1 ^ (-1 << StepBits)
-	n.sequenceShift = NodeBits + StepBits
-	n.nodeShift = StepBits
 
-	if n.node < 0 || n.node > n.nodeMax {
-		return nil, errors.New("Node number must be between 0 and " + strconv.FormatInt(n.nodeMax, 10))
+	if n.node < 0 || n.node > nodeMax {
+		return nil, errors.New("Node number must be between 0 and " + strconv.FormatInt(nodeMax, 10))
 	}
 
 	if err := n.update(); err != nil {
@@ -60,15 +53,15 @@ func (n *Node) Generate() (ID, error) {
 	n.mu.Lock()
 	defer n.mu.Unlock()
 
-	n.step = (n.step + 1) & n.stepMask
+	n.step = (n.step + 1) & stepMask
 	if n.step == 0 {
 		if err := n.update(); err != nil {
 			return 0, err
 		}
 	}
 
-	r := ID(n.sequence<<n.sequenceShift |
-		(n.node << n.nodeShift) |
+	r := ID((n.sequence << sequenceShift) |
+		(n.node << nodeShift) |
 		(n.step),
 	)
 
@@ -80,7 +73,7 @@ func (n *Node) update() error {
 		N int64 `bson:"n"`
 	}
 
-	filter := bson.D{bson.E{Key: "_id", Value: n.tag}}
+	filter := bson.D{bson.E{Key: "_id", Value: n.kind}}
 	update := bson.D{
 		bson.E{
 			Key: "$inc",
@@ -101,6 +94,8 @@ func (n *Node) update() error {
 	return nil
 }
 
+type ID int64
+
 func (f ID) Int64() int64 {
 	return int64(f)
 }
@@ -116,4 +111,16 @@ func (f ID) String() string {
 func ParseString(id string) (ID, error) {
 	i, err := strconv.ParseInt(id, 10, 64)
 	return ID(i), err
+}
+
+func (f ID) Sequence() int64 {
+	return (int64(f) >> sequenceShift)
+}
+
+func (f ID) Node() int64 {
+	return int64(f) & nodeMask >> nodeShift
+}
+
+func (f ID) Step() int64 {
+	return int64(f) & stepMask
 }
